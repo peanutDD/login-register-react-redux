8efd609389fe88df5da59d582924417a73aeba74
peanutDD <www.smoking.sexy@gmail.com>
2018-09-01 01:32:45 +0800

    achieve: let flashmessage fade out automatically after 5s

--- client/src/components/flash/FlashMessage.js
+++ client/src/components/flash/FlashMessage.js
@@ -2,11 +2,29 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames';
 class FlashMessage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHide: false
+    }
+  }
+
+  componentDidMount() {
+    this.hideFlashMessage();
+  }
+
   static propsTypes = {
     message: PropTypes.object.isRequired,
     deleteFlashMessage: PropTypes.func.isRequired
   }
 
+  hideFlashMessage = () => {
+    setTimeout(() => {
+      this.setState({
+        isHide: true
+      })
+    }, 5000);
+  }
   onClick = () => {
     this.props.deleteFlashMessage(this.props.message.id)
   }
@@ -14,11 +32,14 @@ class FlashMessage extends Component {
     const { type, text } = this.props.message
     console.log(this.props);
     return (
-      <div className= {
-        classnames( 'alert', {
-          'alert-success': type ==='success',
-          'alert-danger': type === 'danger'
-      })}>
+      <div
+        className= {
+          classnames( 'alert', {
+            'alert-success': type ==='success',
+            'alert-danger': type === 'danger'
+        })}
+        style={{ display: this.state.isHide ? 'none' : 'block'}}
+      >
         <button onClick={ this.onClick } className="close"><span>&times;</span></button>
         { text }
       </div>
